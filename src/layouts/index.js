ad4ef98db1408bcb9dea0f9c373552f644e46b24
Alex Mireles <anchises1968@gmail.com>
2018-08-28 13:32:54 -0700

    removed rogue file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
-import logo from './logo.png'
+
 
 import '../styles/index.sass'
 
