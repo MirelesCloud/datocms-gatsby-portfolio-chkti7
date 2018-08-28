95513d46e475d8b88ef36633194a216203e11bab
Alex Mireles <anchises1968@gmail.com>
2018-08-28 16:23:27 -0700

    mobile header css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -52,7 +52,7 @@ const TemplateWrapper = ({ children, data }) => (
             <Link to="/" data-js="toggleSidebar" />
           </div>
           <div className="mobile-header__logo">
-            <Link to="/">{data.datoCmsSite.globalSeo.logoImage}</Link>
+            <Link to="/">{data.datoCmsSite.globalSeo.logo}</Link>
           </div>
         </div>
       </div>
