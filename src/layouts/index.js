147e2f7d99e419fa06e8dc1435412ce989d3c382
Alex Mireles <anchises1968@gmail.com>
2018-09-06 14:18:24 -0700

    map css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -75,8 +75,8 @@ const TemplateWrapper = ({ children, data }) => (
       <div className="sheet__map">
 
         <div className="sheet_inner">
-          <div className="sheet__title">{data.datoCmsLocationMap.title}</div>
-          <div className="sheet__lead"
+          <div className="sheet__map__title">{data.datoCmsLocationMap.title}</div>
+          <div className="sheet__map__lead"
             dangerouslySetInnerHTML={{
               __html: data.datoCmsLocationMap.subTextNode.childMarkdownRemark.html,
             }}
